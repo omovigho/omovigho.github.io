aaa09b54747e4a43a1a7aab88082106caeaec8e9
omovigho <100778865+omovigho@users.noreply.github.com>
2024-11-02 16:35:35 +0100

    Defining the color variation in the canvas

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ document.addEventListener(
     const allSections = document.querySelectorAll('.sect')
     const myName = document.querySelector('.my-name');
     const navLink = document.querySelectorAll('.nav-link');
-    // const sect = document.querySelectorAll('.sect');
-    //const text = 'MrBeast'
+    const sect = document.querySelectorAll('.sect');
+    const text = 'Daniel'
     // console.log('r' == 'R')
     
     console.log(String.fromCharCode(65 + 
@@ -57,7 +57,7 @@ document.addEventListener(
             if(index >= text.length){
                 clearInterval(interval);
             }
-        }, 200)
+        }, 100)
     }
     // scrambleText();
     
@@ -163,3 +163,68 @@ otherSection.forEach(function(section){
 })
 
 })
+
+
+// Loading of the canvas
+window.addEventListener("load", eventWindowLoaded, false);
+function eventWindowLoaded() {
+    canvasApp();
+}
+
+
+function canvasSupport () {
+    return Modernizr.canvas;
+}
+
+
+function canvasApp() {
+    var message = "Welcome to My Digital Space";
+    var theCanvas = document.getElementById("canvasOne");
+    var context = theCanvas.getContext("2d");
+
+    function drawScreen() {
+        //Background
+        context.fillStyle = "rgb(41, 39, 39)";
+        context.fillRect(0, 0, theCanvas.width, theCanvas.height);
+        //Text
+        context.font = "2.7rem impact"
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        var metrics = context.measureText(message);
+        var textWidth = metrics.width;
+        var xPosition = (theCanvas.width/2);
+        var yPosition = (theCanvas.height/2);
+        var gradient = context.createLinearGradient( theCanvas.width/2,0,
+        theCanvas.width/2,theCanvas.height);
+
+        for (var i=0; i < colorStops.length; i++) {
+            var tempColorStop = colorStops[i];
+            var tempColor = tempColorStop.color;
+            var tempStopPercent = tempColorStop.stopPercent;
+            gradient.addColorStop(tempStopPercent,tempColor);
+            tempStopPercent += .015;
+            if (tempStopPercent > 1) {
+                tempStopPercent = 0;
+            }
+            tempColorStop.stopPercent = tempStopPercent;;
+            colorStops[i] = tempColorStop;
+        }
+        context.fillStyle = gradient;
+        context.fillText ( message, xPosition ,yPosition);
+    }
+
+    function gameLoop() {
+        window.setTimeout(gameLoop, 20);
+        drawScreen()
+    }
+
+    // Colors variations
+    var colorStops = new Array(
+        {color:"#FF0000", stopPercent:0},
+        {color:"#FFFF00", stopPercent:.125},
+        {color:"#00FF00", stopPercent:.375},
+        {color:"#0000FF", stopPercent:.625},
+        {color:"#FF00FF", stopPercent:.875},
+        {color:"#FF0000", stopPercent:1});
+        gameLoop();
+}
